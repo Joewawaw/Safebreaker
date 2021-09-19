4424e8c1f069b0f0a17228cce8def8e461d399d9
joewawaw <keshavdeeljur@hotmail.com>
2021-09-19 18:15:44 -0400

    Added comments cahnges

--- safebreaker.py
+++ safebreaker.py
@@ -4,6 +4,7 @@ num_tries = 0
 num_check = 0
 num_x = 0
 
+import secrets
 
 def main():
   print('Welcome to safebreaker')
@@ -11,11 +12,8 @@ def main():
 
   raw_input_str = input('> ')
   print(raw_input_str)
-  
-  #we need to check that its only numbers,
-  #4 digits in length
-  #no repeating digits
-  input_int = int(raw_input_str, base=10)
+
+  input_int = int(raw_input_str)
 
   print(check_input(input_int))
 
@@ -26,6 +24,12 @@ def check_input(input_str):
   input_int = int(input_str)
   return has_doubles(input_int)
 
+def gen_solution():
+  result = 9999
+  while has_doubles(result):
+    result = secrets.randbelow(10000)
+  return result
 
 if __name__ == "__main__":
-  main()
+  print(gen_solution())
+  #main()
