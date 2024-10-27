1deca06fb43bc96e4eab47a89d52fbad39f3669f
Joewawaw <keshav.deeljur@gmail.com>
2024-10-26 22:42:04 -0400

    rewrote the entire code many years later..

--- safebreaker.py
+++ safebreaker.py
@@ -1,35 +1,78 @@
-
-
-num_tries = 0
-num_check = 0
-num_x = 0
-
-import secrets
+#!/usr/bin/env python3
+import random
 
 def main():
-  print('Welcome to safebreaker')
-  #print('Enter \'h\' for help")
+    num_tries = 0
+    print('******** Welcome to Safe Breaker ********\n')
+    #print('Enter h for help')
 
-  raw_input_str = input('> ')
-  print(raw_input_str)
+    #prompt for number of tries
+    while True:
+        print('Enter the number of tries [default 10]: ')
+        num_tries = input('> ')
+        if num_tries == '':
+            num_tries = 10
+            break
+        num_tries = int(num_tries)
+        if num_tries <= 4 or num_tries > 20:
+            print(f'Number of tries {num_tries} is invalid! Try again')
+        else:
+            break
 
-  input_int = int(raw_input_str)
+    code_solution = gen_n_digit_code(4)
+    #print(f'{code_solution=}')
 
-  print(check_input(input_int))
+    while num_tries > 0:
+        while True:
+            print('Guess a sequence')
+            code_guess = input('> ')
+            if check_input(code_guess) is True:
+                break
+            else:
+                print('Incorrect input! try again')
+        num_tries -=1
 
-def has_doubles(n):
-    return len(set(str(n))) < len(str(n))
+        num_v, num_x = check_code(code_guess, code_solution)
+        #print (f'V={num_v}\tX={num_x}\tTries left: {num_tries}')
 
-def check_input(input_str):
-  input_int = int(input_str)
-  return has_doubles(input_int)
+        print('  ',end='')
+        for i in range(num_v):
+            print('V',end='')
+        for i in range(num_x):
+            print('X',end='')
+        print(f'\tTries left: {num_tries}')
 
-def gen_solution():
-  result = 9999
-  while has_doubles(result):
-    result = secrets.randbelow(10000)
-  return result
+        if num_v == 4:
+            print(f'Safe Opened! The code was {code_solution}\n')
+            return
+        if num_tries == 0:
+            print(f'Game Over! Answer: {code_solution}\n')
 
-if __name__ == "__main__":
-  print(gen_solution())
-  #main()
+def check_code(guess, solution):
+    num_v = 0
+    num_x = 0
+    for i in range(4):
+        if guess[i] == solution[i]:
+            num_v += 1
+        elif guess[i] in solution:
+            num_x += 1
+    return num_v, num_x
+
+def check_input(s):
+    #this checks if the code has repeating digits
+    if len(s) != len(set(s)):
+        return False
+    #this check is the string is exactly 4 characters
+    if len(s) != 4:
+        return False
+    #this check if there are only digits in the string
+    if not all(char in '0123456789' for char in s):
+        return False
+    return True
+
+def gen_n_digit_code(n):
+    digits = '0123456789'
+    return ''.join(random.sample(digits, 4))
+
+if __name__ == '__main__':
+    main()
